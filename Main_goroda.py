0925097f19a0be028c24a4440b07cf42c6b473fc
Saratsin-25 <7422037@gmail.com>
2024-11-27 22:56:05 +0300

    Округляем координаты

--- Main_goroda.py
+++ Main_goroda.py
@@ -4,16 +4,19 @@ from opencage.geocoder import OpenCageGeocode
 def get_coordinates(city,key):
     try:
         geocoder = OpenCageGeocode(key)
-        results = geocoder.geocode(city, language=ru)
+        results = geocoder.geocode(city, language='ru')
         if results:
-            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+            lat=round(results[0]['geometry']['lat'],2)
+            lon=round(results[0]['geometry']['lng'],2)
+
+            return lat, lon
         else:
             return "Город не найден"
     except Exception as e:
         return f"Возникла ошибка: {e}"
 
 key = 'efa904a5dbcb4b1eb202231dfd647cf7'
-city='London'
+city='Kimry'
 coordinates = get_coordinates(city, key)
 print(f"Координаты города {city}: {coordinates}")
 
